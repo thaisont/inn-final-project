7b7e32c7fac1b4d9f0c52b77c85b0eb5a56f6343
Thai Truong <sontruong@hotmail.co.uk>
2020-11-22 22:26:41 +0000

    new content page added

--- javascript/app.js
+++ javascript/app.js
@@ -8,12 +8,10 @@ burgerNav.addEventListener("click", function () {
   burgerDisplay.classList.toggle("burger-active");
   burgerOutside.classList.toggle("burger__outside--active");
   burgerNav.classList.toggle("animate");
-  body.classList.toggle("overflow");
 });
 
 burgerOutside.addEventListener("click", function (event) {
   burgerDisplay.classList.remove("burger-active");
   burgerOutside.classList.remove("burger__outside--active");
   burgerNav.classList.remove("animate");
-  body.classList.toggle("overflow");
 });
