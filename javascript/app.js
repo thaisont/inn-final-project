858109033ffe8b1649f4a41c20422a3524a0c787
Thai Truong <sontruong@hotmail.co.uk>
2020-11-28 12:04:35 +0000

    Google maps api added

--- javascript/app.js
+++ javascript/app.js
@@ -15,3 +15,40 @@ burgerOutside.addEventListener("click", function (event) {
   burgerOutside.classList.remove("burger__outside--active");
   burgerNav.classList.remove("animate");
 });
+
+//Google Map
+
+// function initMap() {
+
+//   let options = {
+//     zoom: 8,
+//     center {lat:51.5045, lng:0.0865},
+//   }
+
+//   let map = new google.maps.Map(document.getElementById("map"), options)
+// }
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 51.5045, lng: -0.0865 },
+    zoom: 16,
+    mapId: "4a22f8a771c6f75",
+  });
+
+  //Marker
+  const marker = new google.maps.Marker({
+    position: { lat: 51.5045, lng: -0.0865 },
+    map,
+    title: "INN",
+  });
+
+  //info window
+
+  const infowindow = new google.maps.InfoWindow({
+    content: "INN HQ",
+  });
+
+  marker.addListener("click", function () {
+    infowindow.open(map, marker);
+  });
+}
